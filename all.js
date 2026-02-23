1ab8b64e5c86beacc2c69959f344a2652bc13c0b
ChomperHypeLinux <itschomperhype@gmail.com>
2026-02-23 18:44:05 +0700

    Update (23/2/26)

--- all.js
+++ all.js
@@ -1,7 +1,7 @@
 const dev_name = document.getElementById("name_dev");
 if (
   !dev_name ||
-  dev_name.textContent.trim() != "TikTok: @sungsamtech - galaxyA15"
+  dev_name.textContent.trim() != "REDMIK80Hype"
 ) {
   localStorage.clear();
   sessionStorage.clear();
